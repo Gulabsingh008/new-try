d24b35bc8efc28482ca05805d8ff0066883ae59c
AF <gulabsinghrajput008@gmail.com>
2025-03-01 09:51:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,12 @@
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
 from pyrogram.errors import RPCError
-from pyrogram import idle
+from pyrogram.idle import idle
+from config import API_ID, API_HASH, BOT_TOKEN
 
-
-app = Client(
-    "my_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
-
-import handlers.commands  # All commands in one file
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 if __name__ == "__main__":
-    try:
-        app.start()
-        print("Bot is running...")
-        idle()  # Keeps the bot running
-    except RPCError as e:
-        print(f"Error: {e}")
-    finally:
-        app.stop()
-        print("Bot stopped.")
+    app.start()
+    print("✅ Bot Started Successfully!")
+    idle()  # बॉट को रनिंग रखने के लिए
+    app.stop()
