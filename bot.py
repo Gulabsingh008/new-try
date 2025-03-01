263a4e07b9c2d13071a12eb5a74bdeceb09dd0f5
AF <gulabsinghrajput008@gmail.com>
2025-03-01 09:54:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
-from pyrogram import Client
-from pyrogram.errors import RPCError
-from pyrogram.idle import idle
+from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
